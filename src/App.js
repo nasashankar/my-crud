191ac5e35e0ff4e05da5276263f776bc477b2ada
Shankar <v.p.shankar.19.02.1980@gmail.com>
2024-12-05 10:40:17 +0530

    text change

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>good move</h1>
-        <h4>we will will soon</h4>
+        <h1> Hello Shankar You Will Win At One Day</h1>
       </header>
     </div>
   );
